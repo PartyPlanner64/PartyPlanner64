2b4cdce061869410edab515e7890acc7e505f4df
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-08-21 20:28:09 -0500

    Updates

---
+++ precache-manifest.52f3ac22dd0c43f45c8cce7a24547e56.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bbdcb4a627d6a1346576b5bac35731",
+    "url": "/PartyPlanner64/index.html"
+  },
+  {
+    "revision": "6c800c036b9121acfc9a",
+    "url": "/PartyPlanner64/static/css/2.f34c7ea4.chunk.css"
+  },
+  {
+    "revision": "039a9e3af5b0821331b0",
+    "url": "/PartyPlanner64/static/css/main.35100e7f.chunk.css"
+  },
+  {
+    "revision": "6c800c036b9121acfc9a",
+    "url": "/PartyPlanner64/static/js/2.b93e58c0.chunk.js"
+  },
+  {
+    "revision": "039a9e3af5b0821331b0",
+    "url": "/PartyPlanner64/static/js/main.a24fae95.chunk.js"
+  },
+  {
+    "revision": "40fd1f8147b3030a232c",
+    "url": "/PartyPlanner64/static/js/runtime~main.a49f914f.js"
+  },
+  {
+    "revision": "30df9799d55128f38b78abce51f20bf2",
+    "url": "/PartyPlanner64/static/media/eraser.30df9799.cur"
+  },
+  {
+    "revision": "98f1011a92790971143df23698bc85ee",
+    "url": "/PartyPlanner64/static/media/gameguyduel.98f1011a.png"
+  },
+  {
+    "revision": "cc054f8f109a05edbac28b32233957b7",
+    "url": "/PartyPlanner64/static/media/gameguyduel.cc054f8f.png"
+  },
+  {
+    "revision": "423b0269d7295a229f5d71b5332c08ff",
+    "url": "/PartyPlanner64/static/media/happeningduel3.423b0269.png"
+  },
+  {
+    "revision": "ef6ecdf8599a210ef29337308ad63d31",
+    "url": "/PartyPlanner64/static/media/happeningduel3.ef6ecdf8.png"
+  },
+  {
+    "revision": "b49afd48f6d05380083bfe6bbad89159",
+    "url": "/PartyPlanner64/static/media/logo.b49afd48.png"
+  },
+  {
+    "revision": "523c5544d209e1355aa3df070206f274",
+    "url": "/PartyPlanner64/static/media/logoloading.523c5544.png"
+  },
+  {
+    "revision": "8fbfc590c1bd069e5f041c2aea8265ee",
+    "url": "/PartyPlanner64/static/media/mpllogo.8fbfc590.png"
+  },
+  {
+    "revision": "58dd2a1c6d7861ea261912ba153ac8e3",
+    "url": "/PartyPlanner64/static/media/source-sans-pro-v9-latin-regular.58dd2a1c.woff2"
+  },
+  {
+    "revision": "fdc0354668675e460e18d8e6b880e634",
+    "url": "/PartyPlanner64/static/media/source-sans-pro-v9-latin-regular.fdc03546.woff"
+  }
+]);
